ba6ca8d8cab8c690f92b8ebffe4a2fafc02ba741
edmuc <79590523+edmuc@users.noreply.github.com>
2025-09-01 22:07:29 -0500

    1
    1

---
+++ If.py
@@ -0,0 +1,54 @@
+#Implrementar
+#A= int ("10")
+
+#print (type(A), "y la variable valoR", A)
+#Convertir los tipos de datos Float, Int, String entre si
+#result= A+A
+
+# Estructura de control, Operaciones logicas
+#Booleano, 1,0,
+
+#if(A>5):
+#    print ("Este es el resultado del if verdadero")
+#    print (f"La variable a es igual a: {A}")    
+#else:
+#    print ("La condicion no se ejecuto")
+
+
+X= int ("12")
+Y= int ("55")
+Z= int ("6")
+W= int ("33")
+
+if (X>=Z):
+    print (f"Este el el resultado {X}")
+    print (f"La variable a es igual a: {X}")
+
+else:
+    print ("La condicion no se ejecuto")
+
+
+if (W<W):
+    print ("Verdadero")
+    print (f"La variable a es igual a: {W}")
+else:
+    print ("La condicion es falsa")
+
+
+if (Y==Z):
+    print ("")
+    print (f"La variable a es igual a: {Y}")
+else: 
+    print ("La condicion no se ejecuto")
+    
+if  (X<=W):
+    print ("")
+    print (f"La variable a es igual a: {X}")
+else:
+    print ("La condicion no se ejecuto")
+
+if  (X+Y<=W):
+    print ("")
+    print (f"La variable a es igual a: {W}")
+else:
+    print ("La condicion no se ejecuto")
